4c703e96502218fbcbff1e30b2195cee8e7f3c63
sokmean-mptc <ngonsokmean@mptc.gov.kh>
2021-09-30 17:56:32 +0700

    #

--- inc/RegisterAsset.php
+++ inc/RegisterAsset.php
@@ -17,25 +17,13 @@ class RegisterAsset extends BaseController
 {    
     public function register() {
 		add_action( "init", array( $this, "registerScripts" ) );
-		add_action( "enqueue_block_editor_assets", array( $this, "enqueueEditorAssets" ) );
     }
 
 	public function registerScripts() {
-		// wp_register_style(
-		// 	$this->plugin_name . '-editor-style',
-		// 	$this->plugin_url . 'build/index.css',
-		// 	array( 'wp-edit-blocks' )
-		// );
 		wp_register_script(
 			$this->plugin_name . '-js',
 			$this->plugin_url . 'build/index.js',
-			array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-block-editor' )
+			array( 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill' )
 		);
-	}
-    public function enqueueEditorAssets() {
-		
-		// wp_enqueue_style( $this->plugin_name . '-editor-style' );
-		wp_enqueue_script( $this->plugin_name . '-js' );
-		
 	}
 }
